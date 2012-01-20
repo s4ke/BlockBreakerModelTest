6ea09022a7d316acfc46d8f5ba067fc06b798778
s4ke <braun5@gmx.de>
2012-01-20 14:46:37 +0100

    first commit

---
+++ src/de/hotware/model/generator/test/GeneratorTest.java
@@ -0,0 +1,28 @@
+package de.hotware.model.generator.test;
+
+import de.hotware.blockbreaker.model.Level;
+import de.hotware.blockbreaker.model.generator.LevelGenerator;
+
+public class GeneratorTest {
+	
+	public static void main(String args[]) {
+		int numberOfFailures = 0;
+		int numberOfPasses = 0;
+		long afterTime;
+		long preTime = System.nanoTime();
+		System.out.println("Starting Benchmark using BlockBreakerModel v0.1a...");
+		for(int i = 0; i < 999999; ++i) {
+			Level level = LevelGenerator.createRandomLevel(16);
+			if(level.getReplacementList().size() == 0 || level.checkWin()) {
+				++numberOfFailures;
+			} else {
+				++numberOfPasses;
+			}
+		}
+		afterTime = System.nanoTime();
+		System.out.println("Done.");
+		System.out.println("Number of Failures: " + numberOfFailures + "\nNumber of Passes: " + numberOfPasses);
+		System.out.println("The benchmark took: " + (afterTime - preTime)/1000/1000/(double)1000 + "sec.");
+	}
+
+}
