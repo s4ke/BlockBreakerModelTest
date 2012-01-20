854f57dd0223983325ed3e0ed837ce3fb808f671
s4ke <braun5@gmx.de>
2012-01-20 15:03:08 +0100

    removed binary stuff

--- src/de/hotware/model/generator/test/GeneratorTest.java
+++ src/de/hotware/model/generator/test/GeneratorTest.java
@@ -1,24 +1,30 @@
 package de.hotware.model.generator.test;
 
+import java.util.Scanner;
+
 import de.hotware.blockbreaker.model.Level;
 import de.hotware.blockbreaker.model.generator.LevelGenerator;
 
 public class GeneratorTest {
 	
 	public static void main(String args[]) {
+		int numberOfIterations;
+		System.out.println("How many Iterations?");
+		Scanner sc = new Scanner(System.in);
+		numberOfIterations = sc.nextInt();
 		int numberOfFailures = 0;
 		int numberOfPasses = 0;
 		long afterTime;
 		long preTime = System.nanoTime();
 		System.out.println("Starting Benchmark using BlockBreakerModel v0.1a...");
-		for(int i = 0; i < 999999; ++i) {
+		for(int i = 0; i < numberOfIterations; ++i) {
 			Level level = LevelGenerator.createRandomLevel(16);
 			if(level.getReplacementList().size() == 0 || level.checkWin()) {
 				++numberOfFailures;
 			} else {
 				++numberOfPasses;
 			}
-		}
+		} 
 		afterTime = System.nanoTime();
 		System.out.println("Done.");
 		System.out.println("Number of Failures: " + numberOfFailures + "\nNumber of Passes: " + numberOfPasses);
